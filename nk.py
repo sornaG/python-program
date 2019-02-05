44962293446ea59ea26a71759d2ce386f1337b6c
sornaG <37236140+sornaG@users.noreply.github.com>
2019-02-05 12:46:37 +0530

    Create nk.py

---
+++ nk.py
@@ -0,0 +1,11 @@
+
+
+for (int i=0; i < array.size(); i++){
+  int value = array[i];
+  int diff = sum - value; 
+  if (! hashSet.contains(diffvalue)){
+      hashSet.put(value,value);
+  } else{
+       printf(sum = diffvalue + hashSet.get(diffvalue));
+  } 
+}
